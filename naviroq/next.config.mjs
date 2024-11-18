d28897dbec51e72925fa66338ea1b7976e838fa4
Yusuf <y.abdulwasiu@gmail.com>
2024-11-19 00:48:37 +0100

    feat: :sparkles: fix CORS

--- naviroq/next.config.mjs
+++ naviroq/next.config.mjs
@@ -21,35 +21,10 @@ const nextConfig = {
       },
     ],
   },
-  // enable cors
-  async headers() {
-    return [
-      {
-        source: '/api/:path*',
-        headers: [
-          {
-            key: 'Access-Control-Allow-Origin',
-            value:
-              process.env.NODE_ENV === 'production'
-                ? 'https://naviroq.vercel.app'
-                : '*',
-          },
-        
-          { key: 'Access-Control-Allow-Origin', value: '*' },
-          { key: 'Access-Control-Allow-Methods', value: 'GET, POST, PUT, DELETE, PATCH, OPTIONS' },
-          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Authorization, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-          { key: 'Access-Control-Allow-Credentials', value: 'true' },
-        ],
-      },
-    ];
-  },
-
   eslint: {
     ignoreDuringBuilds: true,
   },
   crossOrigin: 'anonymous',
-  
-
 };
 
 export default nextConfig;
